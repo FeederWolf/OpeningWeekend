dc8f6fcdb54f19292fda1a1fe4a96a9661ebec6f
FeederWolf <91455735+FeederWolf@users.noreply.github.com>
2022-03-16 12:49:05 +0100

    1.-2. feladat

--- OpeningWeekend/Class1.cs
+++ OpeningWeekend/Class1.cs
@@ -6,11 +6,8 @@ using System.Threading.Tasks;
 
 namespace OpeningWeekend
 {
-    class Program
+    class Class1
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+
     }
 }
